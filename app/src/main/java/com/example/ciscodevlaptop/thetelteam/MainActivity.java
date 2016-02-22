5501af6c245df30ca122d2b2eda05b69c0786ba6
Francisco Massanet-Nicolau <francisco.massanet-nicolau@leedscitycollege.ac.uk>
2016-02-22 13:57:30 +0000

    22-02-16

---
+++ app/src/main/java/com/example/ciscodevlaptop/thetelteam/MainActivity.java
@@ -0,0 +1,64 @@
+package com.example.ciscodevlaptop.TheTELTeam;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.Window;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    WebView w;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        w=(WebView) findViewById(R.id.webview1);
+        w.getSettings().setJavaScriptEnabled(true);
+        String url=("http://telteam.wix.com/tel-team");
+        w.setWebViewClient(new WebViewClient());
+        w.loadUrl(url);
+
+
+
+    }
+
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && w.canGoBack()) {
+            w.goBack();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
+
+
